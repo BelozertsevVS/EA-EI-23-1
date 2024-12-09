cc116774e6e3f39a197aa3930a8eee1d40432435
aznivlalayan <aznivlalayanner@gmail.com>
2024-12-09 13:47:51 +0200

    Update LalayanLW14.sql

--- ЛР14/LalayanLW14.sql
+++ ЛР14/LalayanLW14.sql
@@ -1,24 +1,23 @@
-
--- Задача 1: Повернути ідентифікатор клієнта (CUST_NUM) з найбільшим кредитним лімітом (CREDIT_LIMIT)
+--- Завдання 1. Запит, який повертає ідентифікатор клієнта (CUST_NUM) з найбільшим кредитним лімітом (CREDIT_LIMIT) ---
 SELECT CUST_NUM
-FROM [dbo].[CUSTOMERS]
-WHERE CREDIT_LIMIT = (SELECT MAX(CREDIT_LIMIT) FROM [dbo].[CUSTOMERS]);
+FROM CUSTOMERS
+WHERE CREDIT_LIMIT = (SELECT MAX(CREDIT_LIMIT) FROM CUSTOMERS);
 
--- Задача 2: Повернути список замовлень клієнта з найбільшим кредитним лімітом (CREDIT_LIMIT)
-SELECT o.ORDER_ID, o.CUST_NUM
-FROM [dbo].[ORDERS] o
-WHERE o.CUST_NUM IN (
-    SELECT CUST_NUM
-    FROM [dbo].[CUSTOMERS]
-    WHERE CREDIT_LIMIT = (SELECT MAX(CREDIT_LIMIT) FROM [dbo].[CUSTOMERS])
+--- Завдання 2. Запит, що повертає список замовлень клієнта з найбільшим кредитним лімітом (CREDIT_LIMIT) ---
+SELECT O.ORDER_NUM, O.ORDER_DATE, O.MFR, O.PRODUCT, O.QTY, O.AMOUNT
+FROM ORDERS O
+WHERE O.CUST IN (
+    SELECT CUST
+    FROM CUSTOMERS
+    WHERE CREDIT_LIMIT = (SELECT MAX(CREDIT_LIMIT) FROM CUSTOMERS)
 );
 
--- Задача 3: Повернути найновіше замовлення клієнта з найбільшим кредитним лімітом (CREDIT_LIMIT)
-SELECT TOP 1 o.ORDER_ID, o.CUST_NUM, o.ORDER_DATE
-FROM [dbo].[ORDERS] o
-WHERE o.CUST_NUM IN (
-    SELECT CUST_NUM
-    FROM [dbo].[CUSTOMERS]
-    WHERE CREDIT_LIMIT = (SELECT MAX(CREDIT_LIMIT) FROM [dbo].[CUSTOMERS])
+--- Завдання 3. Запит, що повертає найновіше (за датою ORDER_DATE) замовлення клієнта з найбільшим кредитним лімітом (CREDIT_LIMIT) ---
+SELECT TOP 1 O.ORDER_NUM, O.ORDER_DATE, O.CUST, O.MFR, O.PRODUCT, O.QTY, O.AMOUNT
+FROM ORDERS O
+WHERE O.CUST IN (
+    SELECT CUST
+    FROM CUSTOMERS
+    WHERE CREDIT_LIMIT = (SELECT MAX(CREDIT_LIMIT) FROM CUSTOMERS)
 )
-ORDER BY o.ORDER_DATE DESC;
+ORDER BY O.ORDER_DATE DESC;
