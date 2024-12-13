23fb9a60f3f35ee050cac385cdabbef4b4629238
SonyaBaR <sonyaba4@gmail.com>
2024-12-13 18:14:26 +0200

    Baluta-LW7

---
+++ ЛР7/Baluta-LW7.sql
@@ -0,0 +1,20 @@
+﻿SELECT *
+FROM [dbo].[SALESREPS];
+SELECT 
+    EMPL_NUM AS EmployeeID,      
+    NAME AS EmployeeName,        
+    AGE,                        
+    TITLE AS Position            
+FROM [dbo].[SALESREPS]
+WHERE AGE > 45
+ORDER BY AGE DESC;
+
+SELECT DISTINCT MFR, PRODUCT
+FROM [dbo].[ORDERS]
+WHERE YEAR(Order_Date) = 2008;
+
+SELECT TOP 1 WITH TIES REP, COUNT(*) AS OrderCount
+FROM [dbo].[ORDERS]
+WHERE YEAR(Order_Date) = 2008
+GROUP BY REP
+ORDER BY COUNT(*) DESC;
